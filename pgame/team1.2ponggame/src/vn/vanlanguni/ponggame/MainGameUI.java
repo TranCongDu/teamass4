f80c807ce54c2d87ebdc9141407594d6de82f103
TranCongDu <clonekd4@gmail.com>
2016-11-11 13:53:35 +0700

    setup eclipse project

---
+++ pgame/team1.2ponggame/src/vn/vanlanguni/ponggame/MainGameUI.java
@@ -0,0 +1,38 @@
+/*
+ * 
+ * 
+ * 
+ * 
+ */
+package vn.vanlanguni.ponggame;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+/**
+ * 
+ * @author Invisible Man
+ *
+ */
+public class MainGameUI extends JFrame{
+	private static final int _HEIGHT = 500;
+	private static final int _WIDTH = 500;
+	private PongPanel pongPanel;
+	
+	public MainGameUI(){
+		setPreferredSize(new Dimension(_WIDTH, _HEIGHT));
+		setLayout(new BorderLayout());
+		setTitle("Pong Game - K21T Ltd.");
+setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pongPanel = new PongPanel();
+		getContentPane().add(pongPanel, BorderLayout.CENTER);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+	}
+
+    public static void main(String[] args) {
+       MainGameUI mainFrame = new MainGameUI();
+       mainFrame.setVisible(true);
+    }
+}
